a19561caca306944505ea593e7a48a3acdd618f0
s2b130050131067 <jayraj.patel16@gmail.com>
2016-02-13 21:30:53 +0530

    4.2, 4.4, 4.8, 4.12, 4.13

---
+++ Prac4/Practical4.13/CheckDetails.java
@@ -0,0 +1,39 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class CheckDetails extends HttpServlet{
+	public void init() throws ServletException{
+		
+	}
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+		
+		String uname = request.getParameter("uname");
+		String password = request.getParameter("password");
+		
+		PrintWriter out = response.getWriter();
+		
+		if(uname.equals("Krut")){
+			if(password.equals("pass")){
+				response.sendRedirect("Authenticated?uname=Krut");
+			}else{
+				response.sendRedirect("loginForm.htm");
+			}
+		}else{
+			response.sendRedirect("loginForm.htm");
+		}
+	}
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+	
+	public void destroy(){
+		
+	}
+  
+}
